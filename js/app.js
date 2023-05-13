92b3616d3507f900d6742b22aafe6dccd49d31d5
faizaghayas <faizaghayas16@gmail.com>
2023-05-13 03:11:02 -0700

    portfolio

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// var bars=document.querySelector(".bars")
+// bars.onclick = function () {
+//     var navbar=document.querySelector(".navbar");
+//     navbar.classList.toggle("active");
+// }
+bars=document.querySelector(".bars");
+bars.onclick = function () {
+    navbar=document.querySelector(".navbar");
+    navbar.classList.toggle("active");
+}
